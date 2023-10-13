315e1066cea253ddcfdfeb4f67ad8c8537f86109
wodahS21 <yaelelyolo@gmail.com>
2023-10-13 09:35:13 -0600

    insercion de mimetypes

--- servidor.js
+++ servidor.js
@@ -9,4 +9,15 @@ var filPath = '.' + request.url;
 if (filePath =='./'){
     filePath = './index.html';
 }
+var extname = String(path.extname(filePath)).toLowerCase();
+var contentType = ('text/html');
+var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+};
 })
