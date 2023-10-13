14ff8f44c3d56e33bbff4ffe2ee31998f7ca73ba
wodahS21 <yaelelyolo@gmail.com>
2023-10-13 09:59:24 -0600

    Manejo de errores con filePath en backend

--- servidor.js
+++ servidor.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+const { type } = require('os');
 
 http.createServer(function(request, response){
 console.log('request', request.url);
@@ -20,4 +21,27 @@ var mimeTypes = {
     '.jpg': 'image/jpg',
     '.gif': 'image/gif',
 };
-})
+contentType = mimeTypes[extname] || 'application/octet-stream';
+//Codigo para manejo de errores con filePath
+fs.readFile(filePath, function(error, content){
+    if (error){
+        if(error.code == 'ENOENT'){
+            fs.readFile('./404.html', function(error, content){
+                response.writeHead(200, { 'Content-Type': contentType});
+                response.end(content, 'utf-8');
+            });
+        }
+        else{
+            response.writeHead(500);
+            response.end('HIJOLE YO CREO QUE NO SE VA A PODER JOVEN, estamos revisando el error: '+error.code+' ..\n');
+            response.end();
+        }
+    }
+    else{
+        response.writeHead(200, {'content-type': contentType});
+        response.end(content, 'utf-8');
+    }
+});
+
+}).listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
